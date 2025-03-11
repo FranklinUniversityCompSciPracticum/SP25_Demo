03356a64407e910c4bd2adb717eaaaa2bc7ce77b
Brian L. Gorman <blgorman@gmail.com>
2025-03-10 21:57:59 -0500

    updates

--- SimpleDemoWeb/SimpleDemoWeb/Data/ApplicationDbContext.cs
+++ SimpleDemoWeb/SimpleDemoWeb/Data/ApplicationDbContext.cs
@@ -1,13 +1,36 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace SimpleDemoWeb.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        private static IConfigurationRoot _configuration;
+        public ApplicationDbContext()
+        {
+            //blank
+        }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            //blank
         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+                _configuration = builder.Build();
+                var cnstr = _configuration.GetConnectionString("FuelAdvisorDb");
+                optionsBuilder.UseSqlServer(cnstr);
+            }
+        }
+
+
     }
 }
